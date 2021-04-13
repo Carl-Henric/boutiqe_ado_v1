0e57968d057ef440465c84911d850ff12191233a
Carl-Henric <carlhenric.grip@gmail.com>
2021-04-13 13:09:10 +0000

    Fix stripe field bug

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -4,7 +4,7 @@
     CSS from here: 
     https://stripe.com/docs/stripe-js
 */
-
+console.log('loaded stripe stuff');
 var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
